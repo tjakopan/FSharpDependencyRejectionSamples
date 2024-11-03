361f363d36730096833e1201b976c54346cbd4e5
Tomislav Jakopanec <tjakopan@gmail.com>
2024-11-03 16:54:44 +0100

    Initial commit

--- BookingApi/Composition.fs
+++ BookingApi/Composition.fs
@@ -1,2 +1,15 @@
 module BookingApi.Composition
 
+module DB =
+  let readReservations connectionString date = []
+  let createReservation connectionString reservation = 0
+
+let connectionString = ""
+
+let flip f x y = f y x
+
+let tryAcceptComposition (reservation: MaitreD.Reservation) =
+  reservation.Date
+  |> DB.readReservations connectionString
+  |> flip (MaitreD.tryAccept 10) reservation
+  |> Option.map (DB.createReservation connectionString)
--- BookingApi/MaitreD.fs
+++ BookingApi/MaitreD.fs
@@ -1,2 +1,16 @@
 module BookingApi.MaitreD
 
+open System
+
+type Reservation =
+  { Date: DateTimeOffset
+    Quantity: int
+    IsAccepted: bool }
+
+let tryAccept capacity reservations reservation =
+  let reservedSeats = reservations |> List.sumBy (_.Quantity)
+
+  if reservedSeats + reservation.Quantity <= capacity then
+    { reservation with IsAccepted = true } |> Some
+  else
+    None
